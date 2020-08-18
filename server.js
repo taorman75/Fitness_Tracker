b624aa262b8a0513aa630819b09f4c8dea0775de
Queen of Corgis <tana.owens@gmail.com>
2020-08-18 10:05:16 -0600

    edits to server.js for heroku

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const mongojs = require("mongojs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(logger("dev"));
 
-mongoose.connect('mongodb://localhost/workouts', {
-  useNewUrlParser: true,
-  useFindAndModify: false
-})
+// mongoose.connect('mongodb://localhost/workouts', {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,7 +20,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //if deployed, use the deployed database. otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workouts";
 
 //connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
